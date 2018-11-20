1cc11834ae741e9f2677cd3886ed47021a099498
Zubairuddin Mujahid <zmujahid@aggies.ncat.edu>
2018-11-19 19:14:24 -0500

    Push2

---
+++ Goodbye163.java
@@ -0,0 +1,9 @@
+/* Class to implement Goodbye163. */
+public class Goodbye163 {
+/* Main Method.
+ * @return print line "Goodbye COMP 163." 
+ */
+  public static void main(String[] args) { 
+    System.out.println("Goodbye COMP 163.");
+  }
+}
